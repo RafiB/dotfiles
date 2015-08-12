6887244b451f59e24df7b0686bb0edb6b01207ac
Rafi Blecher <rafiblecher@gmail.com>
2015-08-13 00:24:51 +1000

    Fix zshrc, add -y to start_mint.sh

--- script/start_mint.sh
+++ script/start_mint.sh
@@ -2,26 +2,29 @@
 sudo apt-get purge banshee libreoffice-* hexchat* pidgin* thunderbird* gimp* brasero*
 sudo add-apt-repository -y ppa:klaus-vormweg/awesome > /dev/null 2>&1
 sudo apt-get update
-sudo apt-get install awesome awesome-extra lua5.1 feh xfce4-power-manager
-sudo apt-get install clang cmake
-sudo apt-get install build-essential
-sudo apt-get install cabal-install ghc
-sudo apt-get install chromium-browser
-sudo apt-get install clementine
+sudo apt-get -y install awesome awesome-extra lua5.1 feh xfce4-power-manager
+sudo apt-get -y install clang cmake
+sudo apt-get -y install build-essential
+sudo apt-get -y install cabal-install ghc
+sudo apt-get -y install chromium-browser
+sudo apt-get -y install clementine
 sudo add-apt-repository -y ppa:git-core/ppa > /dev/null 2>&1
 sudo apt-get update
-sudo apt-get install git gitg bash-completion
-sudo apt-get install mosh
-sudo apt-get install nemo-dropbox
-sudo apt-get install pgf texlive-latex-extra texlive-xetex lmodern
-sudo apt-get install python-pip python-virtualenv python-dev pylint virtualenvwrapper
-sudo apt-get install redshift
-sudo apt-get install silversearcher-ag
+sudo apt-get -y install git gitg bash-completion
+sudo apt-get -y install mosh
+sudo apt-get -y install nemo-dropbox
+sudo apt-get -y install pgf texlive-latex-extra texlive-xetex lmodern
+sudo apt-get -y install python-pip python-virtualenv python-dev pylint virtualenvwrapper
+sudo apt-get -y install redshift
+sudo apt-get -y install silversearcher-ag
 sudo add-apt-repository -y ppa:shutter/ppa > /dev/null 2>&1
 sudo apt-get update
-sudo apt-get install shutter
-sudo apt-get install vim exuberant-ctags
-sudo apt-get install zsh
+sudo apt-get -y install shutter
+sudo add-apt-repository -y ppa:pi-rho/dev > /dev/null 2>&1
+sudo apt-get update
+sudo apt-get install -y tmux
+sudo apt-get -y install vim exuberant-ctags
+sudo apt-get -y install zsh
 
 # Change default shell to zsh
 echo "Changing default shell to zsh for user rafi"
